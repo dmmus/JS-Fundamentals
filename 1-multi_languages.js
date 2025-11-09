04520208e24b5a694e92eeb7faac37230de7f32f
Donat MUSEME <musemeprince@gmail.com>
2025-11-09 21:01:50 +0100

    task 1 3 languages modified

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,3 +1,10 @@
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing!");
+// console.log("C is fun");
+// console.log("Python is cool");
+// console.log("JavaScript is amazing!");
+const line1 = "C is fun";
+const line2 = "Python is cool";
+const line3 = "JavaScript is amazing";
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
