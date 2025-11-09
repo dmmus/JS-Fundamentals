ffe16fcea4e08c3a0f7f0f87d77856ca56290c8f
Donat MUSEME <musemeprince@gmail.com>
2025-11-09 20:48:43 +0100

    task 3 Value of my argument modified

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -2,5 +2,5 @@ const argv=process.argv.slice(2); // remove first two default args
 if (argv.length ===  0) {
     console.log("No argument");
 } else {
-    console.log(argv)
+    console.log(argv[0]);
 }
